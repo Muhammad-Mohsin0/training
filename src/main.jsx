159f55b72c86fcf99bbda14f4eef2c53f6a2fe55
Muhammad-Mohsin0 <mohsin.signin00@gmail.com>
2025-10-16 08:33:52 -0700

    fix bug

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
-import store from "./components/redux/Store.jsx";
+import store from "./components/redux/store.jsx";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
